8b2aab8c437a5300ce40f5d2cf90a25ac5d9a54a
bendyoni <bendyoni@gmail.com>
2025-05-19 01:01:42 +0300

    I introduced an open list into DFS and removed the limit on the number of recursions.

--- Puzzle_Node/DFS.py
+++ Puzzle_Node/DFS.py
@@ -1,31 +1,37 @@
 import heapq
+import sys
 from utils import reconstruct_path ,manhattan_distance
 
-def dfs(start_node, max_depth = 20):
+def dfs(start_node):
+
     closed_list = set()
+    open_list = set()
 
     def recurs(node):
+
         state = tuple(node.board)
 
         if state in closed_list:
             return None 
-        if max_depth < node.depth:
-            return None       
-        closed_list.add(state)
-
+        if state in open_list:
+            return None 
+        
+        open_list.add(state)
+        
         if node.is_goal():
             return node # returns the final node for route reconstruction
-    
+        
         for neighbor in node.get_neighbors():
             neighbor_state = tuple(neighbor.board)
-            if neighbor_state in closed_list:
+            if neighbor_state in closed_list or neighbor_state in open_list:
                 continue
 
             result = recurs(neighbor)
             if result:
                 return result
-            
-        closed_list.remove(state)   
+  
+        open_list.remove(state) 
+        closed_list.add(state)  
         return None   # No route found from the current intersection
 
     goal_node = recurs(start_node)
